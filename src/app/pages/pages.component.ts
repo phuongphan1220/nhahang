c4f846cd8310270a3c19121addef4d7c87c06e1d
QUYEN LY <quyen.ly.151@outlook.com>
2021-05-25 04:35:21 +0700

    clear some component

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-pages',
-  templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.scss']
+  selector: "app-pages",
+  templateUrl: "./pages.component.html",
+  styleUrls: ["./pages.component.scss"],
 })
 export class PagesComponent implements OnInit {
+  public url: any;
 
-  public url : any; 
-
-  constructor(private router: Router) {  
+  constructor(private router: Router) {
     this.router.events.subscribe((event) => {
-          if (event instanceof NavigationEnd) {
-            this.url = event.url;
-          }
+      if (event instanceof NavigationEnd) {
+        this.url = event.url;
+      }
     });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
