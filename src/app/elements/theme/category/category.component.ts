b15cf1582bb996659319bf674a5afbdb4e95e0ac
QUYEN LY <quyen.ly.151@outlook.com>
2021-05-25 20:00:47 +0700

    add image

--- src/app/elements/theme/category/category.component.ts
+++ src/app/elements/theme/category/category.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { CategorySlider } from '../../../shared/data/slider';
+import { Component, OnInit } from "@angular/core";
+import { CategorySlider } from "../../../shared/data/slider";
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  selector: "app-category",
+  templateUrl: "./category.component.html",
+  styleUrls: ["./category.component.scss"],
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   public CategorySliderConfig: any = CategorySlider;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
