9abc7108c8f62a6ffc7bc9de008421bd1120b548
QUYEN LY <quyen.ly.151@outlook.com>
2021-05-25 20:32:40 +0700

    3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { ShopComponent } from './shop/shop.component';
-import { PagesComponent } from './pages/pages.component';
-import { ElementsComponent } from './elements/elements.component';
+import { ShopComponent } from "./shop/shop.component";
+import { ElementsComponent } from "./elements/elements.component";
 
 const routes: Routes = [
   {
@@ -21,12 +20,6 @@ const routes: Routes = [
     loadChildren: () => import("./shop/shop.module").then((m) => m.ShopModule),
   },
   {
-    path: "pages",
-    component: PagesComponent,
-    loadChildren: () =>
-      import("./pages/pages.module").then((m) => m.PagesModule),
-  },
-  {
     path: "elements",
     component: ElementsComponent,
     loadChildren: () =>
@@ -39,13 +32,15 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled',
-    useHash: false,
-    anchorScrolling: 'enabled',
-    scrollPositionRestoration: 'enabled',
-    relativeLinkResolution: 'legacy'
-})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: "enabled",
+      useHash: false,
+      anchorScrolling: "enabled",
+      scrollPositionRestoration: "enabled",
+      relativeLinkResolution: "legacy",
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
