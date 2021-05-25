1a20049ad10fce6a3ffe7c4db6702fb21d62cdc7
QUYEN LY <quyen.ly.151@outlook.com>
2021-05-25 20:26:24 +0700

    1

--- src/app/shop/checkout/checkout.component.ts
+++ src/app/shop/checkout/checkout.component.ts
@@ -1,44 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { IPayPalConfig, ICreateOrderRequest } from 'ngx-paypal';
-import { environment } from '../../../environments/environment';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
+import { IPayPalConfig, ICreateOrderRequest } from "ngx-paypal";
+import { environment } from "../../../environments/environment";
 import { Product } from "../../shared/classes/product";
 import { ProductService } from "../../shared/services/product.service";
 import { OrderService } from "../../shared/services/order.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  selector: "app-checkout",
+  templateUrl: "./checkout.component.html",
+  styleUrls: ["./checkout.component.scss"],
 })
 export class CheckoutComponent implements OnInit {
-
-  public checkoutForm:  FormGroup;
+  public checkoutForm: FormGroup;
   public products: Product[] = [];
-  public payPalConfig ? : IPayPalConfig;
-  public payment: string = 'Stripe';
-  public amount:  any;
+  public payPalConfig?: IPayPalConfig;
+  public payment: string = "Stripe";
+  public amount: any;
 
-  constructor(private fb: FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     public productService: ProductService,
-    private orderService: OrderService) { 
+    private orderService: OrderService,
+    private router: Router
+  ) {
     this.checkoutForm = this.fb.group({
-      firstname: ['', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z ]+[a-zA-Z]$')]],
-      lastname: ['', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z ]+[a-zA-Z]$')]],
-      phone: ['', [Validators.required, Validators.pattern('[0-9]+')]],
-      email: ['', [Validators.required, Validators.email]],
-      address: ['', [Validators.required, Validators.maxLength(50)]],
-      country: ['', Validators.required],
-      town: ['', Validators.required],
-      state: ['', Validators.required],
-      postalcode: ['', Validators.required]
-    })
+      firstname: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("[a-zA-Z][a-zA-Z ]+[a-zA-Z]$"),
+        ],
+      ],
+      lastname: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("[a-zA-Z][a-zA-Z ]+[a-zA-Z]$"),
+        ],
+      ],
+      phone: ["", [Validators.required, Validators.pattern("[0-9]+")]],
+      email: ["", [Validators.required, Validators.email]],
+      address: ["", [Validators.required, Validators.maxLength(50)]],
+      country: ["", Validators.required],
+      town: ["", Validators.required],
+      state: ["", Validators.required],
+      postalcode: ["", Validators.required],
+    });
   }
 
   ngOnInit(): void {
-    this.productService.cartItems.subscribe(response => this.products = response);
-    this.getTotal.subscribe(amount => this.amount = amount);
+    this.productService.cartItems.subscribe(
+      (response) => (this.products = response)
+    );
+    this.getTotal.subscribe((amount) => (this.amount = amount));
     this.initConfig();
   }
 
@@ -48,70 +65,76 @@ export class CheckoutComponent implements OnInit {
 
   // Stripe Payment Gateway
   stripeCheckout() {
-    var handler = (<any>window).StripeCheckout.configure({
-      key: environment.stripe_token, // publishble key
-      locale: 'auto',
-      token: (token: any) => {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-        this.orderService.createOrder(this.products, this.checkoutForm.value, token.id, this.amount);
-      }
-    });
-    handler.open({
-      name: 'Multikart',
-      description: 'Online Fashion Store',
-      amount: this.amount * 100
-    }) 
+    alert("CheckOut Success");
+    localStorage.clear();
+    this.router.navigate(["home/vegetable"]);
   }
 
   // Paypal Payment Gateway
   private initConfig(): void {
     this.payPalConfig = {
-        currency: this.productService.Currency.currency,
-        clientId: environment.paypal_token,
-        createOrderOnClient: (data) => < ICreateOrderRequest > {
-          intent: 'CAPTURE',
-          purchase_units: [{
+      currency: this.productService.Currency.currency,
+      clientId: environment.paypal_token,
+      createOrderOnClient: (data) =>
+        <ICreateOrderRequest>{
+          intent: "CAPTURE",
+          purchase_units: [
+            {
               amount: {
                 currency_code: this.productService.Currency.currency,
                 value: this.amount,
                 breakdown: {
-                    item_total: {
-                        currency_code: this.productService.Currency.currency,
-                        value: this.amount
-                    }
-                }
-              }
-          }]
+                  item_total: {
+                    currency_code: this.productService.Currency.currency,
+                    value: this.amount,
+                  },
+                },
+              },
+            },
+          ],
+        },
+      advanced: {
+        commit: "true",
       },
-        advanced: {
-            commit: 'true'
-        },
-        style: {
-            label: 'paypal',
-            size:  'small', // small | medium | large | responsive
-            shape: 'rect', // pill | rect
-        },
-        onApprove: (data, actions) => {
-            this.orderService.createOrder(this.products, this.checkoutForm.value, data.orderID, this.getTotal);
-            console.log('onApprove - transaction was approved, but not authorized', data, actions);
-            actions.order.get().then(details => {
-                console.log('onApprove - you can get full order details inside onApprove: ', details);
-            });
-        },
-        onClientAuthorization: (data) => {
-            console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
-        },
-        onCancel: (data, actions) => {
-            console.log('OnCancel', data, actions);
-        },
-        onError: err => {
-            console.log('OnError', err);
-        },
-        onClick: (data, actions) => {
-            console.log('onClick', data, actions);
-        }
+      style: {
+        label: "paypal",
+        size: "small", // small | medium | large | responsive
+        shape: "rect", // pill | rect
+      },
+      onApprove: (data, actions) => {
+        this.orderService.createOrder(
+          this.products,
+          this.checkoutForm.value,
+          data.orderID,
+          this.getTotal
+        );
+        console.log(
+          "onApprove - transaction was approved, but not authorized",
+          data,
+          actions
+        );
+        actions.order.get().then((details) => {
+          console.log(
+            "onApprove - you can get full order details inside onApprove: ",
+            details
+          );
+        });
+      },
+      onClientAuthorization: (data) => {
+        console.log(
+          "onClientAuthorization - you should probably inform your server about completed transaction at this point",
+          data
+        );
+      },
+      onCancel: (data, actions) => {
+        console.log("OnCancel", data, actions);
+      },
+      onError: (err) => {
+        console.log("OnError", err);
+      },
+      onClick: (data, actions) => {
+        console.log("onClick", data, actions);
+      },
     };
   }
-
 }
