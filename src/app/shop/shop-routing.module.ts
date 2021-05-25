66697ce83c489a07c86dc709c785ce72a42b7e4d
QUYEN LY <quyen.ly.151@outlook.com>
2021-05-25 20:30:11 +0700

    2

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -3,11 +3,6 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { ProductLeftSidebarComponent } from "./product/sidebar/product-left-sidebar/product-left-sidebar.component";
 
-import { CollectionLeftSidebarComponent } from "./collection/collection-left-sidebar/collection-left-sidebar.component";
-import { CollectionRightSidebarComponent } from "./collection/collection-right-sidebar/collection-right-sidebar.component";
-import { CollectionNoSidebarComponent } from "./collection/collection-no-sidebar/collection-no-sidebar.component";
-import { CollectionInfinitescrollComponent } from "./collection/collection-infinitescroll/collection-infinitescroll.component";
-
 import { CartComponent } from "./cart/cart.component";
 import { WishlistComponent } from "./wishlist/wishlist.component";
 import { CheckoutComponent } from "./checkout/checkout.component";
@@ -21,22 +16,6 @@ const routes: Routes = [
     resolve: {
       data: Resolver,
     },
-  },
-  {
-    path: "collection/left/sidebar",
-    component: CollectionLeftSidebarComponent,
-  },
-  {
-    path: "collection/right/sidebar",
-    component: CollectionRightSidebarComponent,
-  },
-  {
-    path: "collection/no/sidebar",
-    component: CollectionNoSidebarComponent,
-  },
-  {
-    path: "collection/infinitescroll",
-    component: CollectionInfinitescrollComponent,
   },
   {
     path: "cart",
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -16,20 +16,6 @@ import { SocialComponent } from "./product/widgets/social/social.component";
 import { StockInventoryComponent } from "./product/widgets/stock-inventory/stock-inventory.component";
 import { RelatedProductComponent } from "./product/widgets/related-product/related-product.component";
 
-// Collection Components
-import { CollectionLeftSidebarComponent } from "./collection/collection-left-sidebar/collection-left-sidebar.component";
-import { CollectionRightSidebarComponent } from "./collection/collection-right-sidebar/collection-right-sidebar.component";
-import { CollectionNoSidebarComponent } from "./collection/collection-no-sidebar/collection-no-sidebar.component";
-import { CollectionInfinitescrollComponent } from "./collection/collection-infinitescroll/collection-infinitescroll.component";
-
-// Collection Widgets
-import { GridComponent } from "./collection/widgets/grid/grid.component";
-import { PaginationComponent } from "./collection/widgets/pagination/pagination.component";
-import { BrandsComponent } from "./collection/widgets/brands/brands.component";
-import { ColorsComponent } from "./collection/widgets/colors/colors.component";
-import { SizeComponent } from "./collection/widgets/size/size.component";
-import { PriceComponent } from "./collection/widgets/price/price.component";
-
 import { CartComponent } from "./cart/cart.component";
 import { WishlistComponent } from "./wishlist/wishlist.component";
 import { CheckoutComponent } from "./checkout/checkout.component";
@@ -42,16 +28,6 @@ import { CheckoutComponent } from "./checkout/checkout.component";
     SocialComponent,
     StockInventoryComponent,
     RelatedProductComponent,
-    CollectionLeftSidebarComponent,
-    CollectionRightSidebarComponent,
-    CollectionNoSidebarComponent,
-    CollectionInfinitescrollComponent,
-    GridComponent,
-    PaginationComponent,
-    BrandsComponent,
-    ColorsComponent,
-    SizeComponent,
-    PriceComponent,
     CartComponent,
     WishlistComponent,
     CheckoutComponent,
